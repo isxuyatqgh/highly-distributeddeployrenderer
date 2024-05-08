b85fafb2c43f65431001152dd96f3eb90ec8a42b
isxuyatqgh <gs1shunemac@hotmail.com>
2024-05-08 13:53:57 +0700

    feat task

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-function buildTree(preorder, inorder) {
-  if (preorder.length === 0 || inorder.length === 0) return null;
-  const rootVal = preorder[0];
-  const root = new TreeNode(rootVal);
-  const index = inorder.indexOf(rootVal);
-  const leftInorder = inorder.slice(0, index);
-  const rightInorder = inorder.slice(index + 1);
-  const leftPreorder = preorder.slice(1, 1 + leftInorder.length);
-  const rightPreorder = preorder.slice(1 + leftInorder.length);
-  root.left = buildTree(leftPreorder, leftInorder);
-  root.right = buildTree(rightPreorder, rightInorder);
-  return root;
-}
+const factorial = (n) => {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+};
